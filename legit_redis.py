29af72788a795972d8979c998a70e2f61fc388c8
Riosa; Blake Andrew <brio8688@uni.sydney.edu.au>
2014-04-03 11:25:30 +1100

    tweaks to the puller

--- legit_redis.py
+++ legit_redis.py
@@ -5,14 +5,14 @@ tmpfilename = "tmp.legit"
 logfile = open("legit.log", 'a')
 while True:
     with open(tmpfilename, 'w') as f:
-        subprocess.call("git pull origin master".split(), stdout=f, stderr=f)
+        subprocess.call("git fetch".split(), stdout=f, stderr=f)
 
 
     with open(tmpfilename, 'rU') as f:
         content = f.read()
-        if "already up-to-date" not in content.lower():
+        if content:
             subprocess.call("killall level1".split())
-            logfile.write("[%s] Resetting...\n" %datetime.datetime.now())
+            logfile.write("[%s] Resetting...\nContent=%s\n" % (datetime.datetime.now(), content))
             subprocess.call("git reset --hard origin/master".split())
             subprocess.Popen("nohup ./level1 &".split())
             logfile.write("[%s] Updated repo\n" %datetime.datetime.now())
